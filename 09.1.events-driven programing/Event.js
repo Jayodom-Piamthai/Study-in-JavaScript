9e14925efc01a55d15b7a91f19844223e56308e1
CEEXAGON <acejayodom@gmail.com>
2022-05-28 23:28:04 +0700

    adawd

---
+++ 09.1.events-driven programing/Event.js
@@ -0,0 +1,3 @@
+function Rhongler(){
+    location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
---
+++ 09.5.farentocel but events/function.js
@@ -0,0 +1,24 @@
+function evening(){//tis a function,for ye to pull it to a good use
+    alert("Good evening,stranger")
+}
+evening() //just type the name of the function with (),nothing more,nothing less,and it will be use
+console.log("exit")
+function FarenToCel(){
+    let value = (far-32)*5/9
+    return value.toFixed(2) + " F ํ";//set to only two decimals
+}
+function display(element , value)
+{
+    document.getElementById(element).innerHTML="<b>" + value + "</b>"
+}
+function CelToFaren(){
+    let value = (cel+32)*9/5
+    return value.toFixed(2) + " C ํ";//set to only two decimals
+}
+function displaycel(){
+    alert(displaycel(135))
+}
+
+function displayfaren(){
+    alert(displayfaren(456))
+}
