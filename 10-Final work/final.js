f4b5a67d47be6c93f4586ccdae5735bf1f2e8e58
CEEXAGON <acejayodom@gmail.com>
2022-06-03 22:22:14 +0700

    LOOPED!

--- 10-Final work/final.js
+++ 10-Final work/final.js
@@ -2,4 +2,11 @@ prompt("hi,what's your name")
 prompt("what did i do to you?")
 prompt("when did i do that to you?")
 
-   
+function Rhongler(){
+    i=0
+    while (i < 69){
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        i == i+1
+    }
+    document.getElementById("Faked").innerHTML = "<p>"+"lmao,im not sorry,not one bit. get pranked suckas" + "</p>" ;
+}  
