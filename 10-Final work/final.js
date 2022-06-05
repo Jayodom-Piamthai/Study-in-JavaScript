9cf5b8717fa57c3f2f2e077fff984b75bf1dc5ff
CEEXAGON <acejayodom@gmail.com>
2022-06-05 23:43:24 +0700

    lmao

--- 10-Final work/final.js
+++ 10-Final work/final.js
@@ -1,15 +1,15 @@
 let TheName = prompt("hi,what's your name")
 let Cause = prompt("what did i do to you?")
-let Date = prompt("when did i do that to you?")
+let IncDate = prompt("when did i do that to you?")
 
 function Rhongler(){
     i=0
     while (i < 69){
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-        i == i+1
+        console.log(i)
+        i = i+1
     }
     document.getElementById("Faked").innerHTML = "<p>"+"lmao,im not sorry,not one bit. get pranked suckas" + "</p>" ;
 }  
-document.getElementById("TheirName").innerHTML = TheName
-document.getElementById("TheirName").innerHTML = TheName
-document.getElementById("TheirName").innerHTML = TheName
+document.getElementById("TheName").innerHTML =  "look,["+ TheName + "],im sorry for you,im really,really sorry"
+document.getElementById("CauseNDate").innerHTML = "its been days after i did that to you,i should sorry to you sooner"
