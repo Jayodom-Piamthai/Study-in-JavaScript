b074509624f4239faa0648098736c7b94b45710a
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2022-07-18 22:56:00 +0700

    chap2

---
+++ DOM manipulation/01 element selection.js
@@ -0,0 +1,29 @@
+// GetElementById() /Id
+
+const title = document.getElementById()
+console.log(title)
+
+
+// GetElementByclassname() /class 
+
+const moarStuff = document.getElementsByClassName()
+console.log(title)
+
+
+// GetElementByTagName() /Tag 
+
+const tagged = document.getElementsByTagName()
+console.log(title)
+
+
+// querySelector()  **NOTE:select the first that match the selected tag
+
+const qs = document.querySelector()
+console.log(container)
+
+
+// querySelectorAll() ***NOTE:applied to ALL the member tht use the tag in the line
+
+const qsa = document.querySelectorAll()
+console.log(container)
+//**not quite reccomended to be use on a regular work,the scope's knda bit too beeeg */
